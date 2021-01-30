764f56060311dbedee546c3c53493f7bd43e6aab
MayLevi <maylevi990@gmail.com>
2021-01-30 17:08:02 +0200

    add firebase

--- app/src/main/java/com/example/letsrun/model/Model.java
+++ app/src/main/java/com/example/letsrun/model/Model.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Model {
     public final static Model instance=new Model();
+    ModelFirebase modelFirebase=new ModelFirebase();
+    ModelSql modelSql=new ModelSql();
     private Model(){
 
     }
@@ -14,6 +16,7 @@ public class Model {
         void onComplete(List<User> list);
     }
     public void getAllFriendes(getAllFriendesListener listener){
+        modelFirebase.getAllFriends(listener);
 
     }
     public interface addFriendListener {
@@ -21,6 +24,7 @@ public class Model {
     }
     @SuppressLint("StaticFieldLeak")
     public void addFriend(final User user, addFriendListener listener){
+        modelFirebase.addFriend(user,listener);
     }
 
 }
---
+++ app/src/main/java/com/example/letsrun/model/ModelFirebase.java
@@ -0,0 +1,9 @@
+package com.example.letsrun.model;
+
+public class ModelFirebase {
+    public void getAllFriends(Model.getAllFriendesListener listener) {
+    }
+
+    public void addFriend(User user, Model.addFriendListener listener) {
+    }
+}
